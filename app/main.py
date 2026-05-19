bc892e23fec6884a903ba0f2779a8783b7060d8f
DeanDSomers <deandsomers@gmail.com>
2026-05-19 22:45:15 +0530

    Updated app.main with analyse and version

--- app/main.py
+++ app/main.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
-@app.route("/")
-
+@app.route('/')
 def home():
     return "Welcome to SentinelAI"
 
-app.run()
+@app.route('/analyze')
+def analyse():
+    log = request.args.get('log')
+
+    if 'failed' in log:
+        result = 'Possible brute-force attempt'
+    elif 'virus' in log:
+        result = 'Possible malware detected'
+    else:
+        result = 'No threat detected'
+
+    return {
+        'log':log,
+        'analysis':result
+    }
+
+@app.route('/version')
+def version():
+    return 'SentinelAI version 1.0'
+
+app.run(debug=True)
